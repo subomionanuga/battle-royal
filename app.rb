206e821131f6fc7d5fd620f11854bc462436c731
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2018-12-13 17:26:57 +0000

    View hitpoints - done as a separate page

--- app.rb
+++ app.rb
@@ -10,14 +10,23 @@ class Battle < Sinatra::Base
   end
 
   post "/names" do
-    session['Player_1'] = params[:Player_1]
-    session['Player_2'] = params[:Player_2]
+    session[:Player_1] = params[:Player_1]
+    session[:Player_2] = params[:Player_2]
     redirect '/play'
   end
 
-  get '/play' do
+  get "/play" do
+    @Player_1 = session[:Player_1]
+    @Player_2 = session[:Player_2]
     erb(:play)
   end
+
+  get "/arena" do
+    @Player_1 = session[:Player_1]
+    @Player_2 = session[:Player_2]
+    erb(:arena)
+  end
+
 
   run! if app_file == $0
 
